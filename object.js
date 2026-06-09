893d3915a2d238717c044eab594ff0c1101047e7
Harsh <hs855787@gmail.com>
2026-06-09 10:54:05 +0530

    objects

--- object.js
+++ object.js
@@ -24,19 +24,43 @@
 // // console.log(discount);
 
 
+// const car={
+//     name:"Benz",
+//     color:"black",
+//     model:2026
+// };
+// console.log(car);
+// console.log(car.name);
+// console.log(car["name"]);
+
+// //Update object
+// car.name="ferrari";
+// console.log(car);
+
+// //add property
+// car.horsepower="750cc"
+// console.log(car);
+
+const emptyobj={};
+
+emptyobj.name="alice"
+emptyobj.email="alice123@yahoo.com"
+emptyobj.age=19
+
+console.log(emptyobj);
+//delete a property
+delete emptyobj.age
+console.log(emptyobj);
+
+// model name engie torse hoursepower function we like that car
+
 const car={
     name:"Benz",
-    color:"black",
-    model:2026
-};
+    model:2026,
+    engine:"450cc",
+    like: function() {
+        console.log(`I like ${car.name} because something it has that engine ${car.engine}`);
+    },
+}
 console.log(car);
-console.log(car.name);
-console.log(car["name"]);
-
-//Update object
-car.name="ferrari";
-console.log(car);
-
-//add property
-car.horsepower="750cc"
-console.log(car);
+(car.like());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-console.log(discount);
+const car={
+    name:"Benz",
+    model:2026,
+    engine:"450cc",
+    like: function() {
+        let d=console.log(`I like ${car.name} because something it has that engine ${car.engine}`);
+  return d;
+    },
+}
+console.log(car);
+console.log(car.like());
