6307825cb888fbe70c8bb3bc656fa6ae044157b1
Harsh <hs855787@gmail.com>
2026-07-07 14:45:58 +0530

    let,var,const

---
+++ variables.js
@@ -0,0 +1,12 @@
+const userId =1313
+let userEmail="test@gmail.com"
+var userPassword="12345"
+let accountState;
+
+userCity="mumbai"
+
+userCity="Bombay"
+console.log(userCity)
+console.log([userId,userEmail,userPassword])
+console.table([userId,userEmail,userPassword])
+console.log(accountState)
