97dd773dadbecfbe70a190762ad35b0ebfbae3c0
Harsh <hs855787@gmail.com>
2026-07-08 11:24:10 +0530

    conversionOfdata

---
+++ conversionOperation.js
@@ -0,0 +1,27 @@
+let score ="33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+let g=null;
+let h=undefined
+
+console.log(Number(g))
+console.log(Number(h))
+
+let isLoggedIn="harsh"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1  =  true  and 0 = false
+//"" empty string result in false
+//"harsh" string result in true
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
- // console.log(err);
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
