89c65d22e0b7cb0987b8ee6ecc6107a1b4604c54
Harsh <hs855787@gmail.com>
2026-07-10 23:34:16 +0530

    iife done

--- 3_basics/4_iife.js
+++ 3_basics/4_iife.js
@@ -7,6 +7,7 @@
 
 //semicolun matters more to end iife function it let
 //function know when to stop
+
 ( (name) => {
     //Unnamed IIFE with parameter pass
   console.log(`DB1 Connected ${name}`);
