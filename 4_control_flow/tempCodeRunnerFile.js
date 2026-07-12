3d25afc50ba2df11f59369969233f538d76b07fe
Harsh <hs855787@gmail.com>
2026-07-12 23:45:37 +0530

    Nullish Coalescing and ternary operator done

--- 4_control_flow/tempCodeRunnerFile.js
+++ 4_control_flow/tempCodeRunnerFile.js
@@ -1,18 +1,2 @@
-const month ="Feb";
-
-//use Shift alt arrown down key to duplicate
-switch (month) {
-    case "Jan":
-        console.log("January")
-        break;
-    case "Feb":
-        console.log("Februrary")
-        break;
-    case "Mar":
-        console.log("March")
-        break;
-        
-    default:
-        console.log("Default")
-        break;
-}
+const iceTeaPrice=80
+iceTeaPrice <=50 ? console.log("less then 50") : console.log("More then 80")
--- 4_control_flow/truthy.js
+++ 4_control_flow/truthy.js
@@ -1,8 +1,61 @@
  const userEmail ="h@harsh.ai"
 
+//  if(userEmail){
+//     console.log("got user email")
+//  }
+//  else{
+//     console.log("Dont have user email")
+//  }
+ 
+//  const userEmail =""
+//  if(userEmail){
+//     console.log("got user email")
+//  }
+//  else{
+//     console.log("Dont have user email")
+//  }
+ const userEmail =[];
  if(userEmail){
     console.log("got user email")
  }
  else{
     console.log("Dont have user email")
  }
+ 
+ //falsy values
+
+ //false, 0 ,-0 ,BigInt 0n, "",null,undefined,NaN
+
+ //truthy values
+ //"0" ,'false' ," "-space ,[],{},function()
+
+
+//to check array is Empty
+ if (userEmail.length===0) {
+    console.log("Array is Empty")
+ }
+
+ //to check object is Empty
+const empty0bj ={}
+
+if (Object.keys(empty0bj).length===0) {
+    console.log("object is Empty")
+}
+
+//Nullish Coalescing Operator (??):null undefined
+
+let val1;
+// val1 =5??10 
+// val1=null ??10
+// val1=undefined??15
+val1=null??10??15;
+console.log(val1)
+
+
+//Ternary Operator
+
+// condition ?true :false
+
+const iceTeaPrice=80
+iceTeaPrice <=50 ? console.log("less then 50") : console.log("More then 80")
+
