6fe1dff46b608516e54d0618afec64d80eb4c753
Harsh <hs855787@gmail.com>
2026-07-12 20:21:03 +0530

    conditional statments

---
+++ 4_control_flow/one.js
@@ -0,0 +1,39 @@
+//if
+const isUserLoggedin=true;
+const temperature =41;
+// if(2=="2"){
+//  console.log("hello")
+// }
+// if(2!=3){
+//  console.log("hello")
+// }
+
+// if(temperature<50){
+//     console.log("temperature is less then 50")
+// }
+// console.log("temperature is more then 50")
+
+// if(temperature===40){
+//     console.log("temperature is less then 50")
+// }
+// else{
+// console.log("temperature is more then 50")}
+
+//<, > , <=,>=,==,!=,===,!==
+
+const score=200;
+
+if(score>100){
+    const power="fly";
+console.log(`User Power ${power}`)}
+else{
+
+}
+
+//short hand notation
+const balance =1000
+
+if(balance>500) console.log("test")  //implicit scope but not prefered to Use
+
+//nesting
+ 
---
+++ 4_control_flow/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+if(temperature<50){
+//     console.log("temperature is less then 50")
+// }
+// console.log("temperature is more then 50")
