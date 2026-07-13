f2205a9a4d9d9e5407c9c871e29ffb67c8952db6
Harsh <hs855787@gmail.com>
2026-07-13 22:49:12 +0530

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,13 @@
+//for
+
+
+for (let index = 0; index <10; index++) {
+      console.log(index)
+}
+console.log("")
+let array=[1,23,4,5];
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index])
+    
+}
