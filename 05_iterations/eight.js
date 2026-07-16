3a0ea9da7097744e63fe75c71dc6a5a7755739cd
Harsh <hs855787@gmail.com>
2026-07-16 21:07:20 +0530

    reduce method with example

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -2,5 +2,8 @@ const nums=[1,2,3,4,5]
 const intial=0;
 
 const sumwithintial=nums.reduce((x,y)=>x+y,intial)
-
+//reduce method works with
+// acculmator and currentvalue  we need to define a
+// variable as intialvalue as zero it will help
+//us to calculate total value from array
 console.log(sumwithintial)
