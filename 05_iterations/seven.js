d4aa286eadb4a11081c6360a47ca7d72fd22c8b4
Harsh <hs855787@gmail.com>
2026-07-16 20:48:33 +0530

    maps with foreach example

---
+++ 05_iterations/seven.js
@@ -0,0 +1,8 @@
+const myNums=[1,3,5,7,9,11]
+
+const f=myNums.map((item)=> item+10)
+console.log(f)
+
+const j=myNums.forEach((item)=>console.log(item+10))
+console.log(j)
+
--- 05_iterations/tempCodeRunnerFile.js
+++ 05_iterations/tempCodeRunnerFile.js
@@ -1,21 +1,4 @@
+const myNums=[1,3,5,7,9,11]
 
-
-const books=[
-    {
-     title:'Book One',genre:'Fiction', publish:1997    
-    },
-    {
-    title:'Book Two',genre:'History', publish:2001
-    },
-    {
-     title:'Book Three',genre:'Science', publish:1999  
-    },
-    {
-     title:'Book Four',genre:'Non-Fiction', publish:2005 
-    },
-]
-
-const userBooks=books.filter((item)=> item.publish>2004)
-const userBooksGenre=books.filter((item)=> item.genre==='History'&&item.publish>1995)
-// console.log(userBooks)
-console.log(userBooksGenre)
+const f=myNums.map((item)=> item+10)
+console.log(f)
